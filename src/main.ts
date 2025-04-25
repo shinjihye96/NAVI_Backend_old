e154f9fb945b6360de809ced9b70e721a4df426d
shinjihye <twg0226@gmail.com>
2025-04-26 00:04:03 +0900

    swagger 설정정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Swagger 설정
+  const config = new DocumentBuilder() // 문서 정보 세팅팅
+    .setTitle('하루공유 API')
+    .setDescription('하루공유, 모아보기 API 문서입니다.')
+    .setVersion('1.0')
+    .addTag('DailyShare') // 컨트롤러에서 붙인 @ApiTags()와 맞춤
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); // 실제 문서 생성성
+  SwaggerModule.setup('api-docs', app, document); // api-docs로 문서 열기기
 
   // ✅ CORS 설정
   app.enableCors({
