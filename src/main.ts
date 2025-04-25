efd34363a1df2ab9266ac161ebcf12182423afe1
shinjihye <twg0226@gmail.com>
2025-04-25 23:43:04 +0900

    main 설정정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DailyShareService } from './daily-share/daily-share.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // ✅ Seed 실행
-  const dailyShareService = app.get(DailyShareService);
-  await dailyShareService.seedMockData(); // ← 이거 꼭 필요함!
 
   // ✅ CORS 설정
   app.enableCors({
